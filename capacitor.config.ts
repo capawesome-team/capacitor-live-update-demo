67ab310888dc9b92e7d3b951fc2dbab55d4d2149
Robin Genz <mail@robingenz.dev>
2024-07-29 17:20:14 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor-live-update-demo',
+  webDir: 'www'
+};
+
+export default config;
