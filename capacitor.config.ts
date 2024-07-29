0b9d93d70fb014dc8bbae13403d65a24f979c5bd
Robin Genz <mail@robingenz.dev>
2024-07-29 18:35:45 +0200

    Add implementation

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'capacitor-live-update-demo',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    LiveUpdate: {
+      appId: 'c6dd52ad-c8e9-49cd-bc63-c516d601f17f',
+    }
+  }
 };
 
 export default config;
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { LiveUpdate } from '@capawesome/capacitor-live-update';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
   constructor() {}
+
+  public onClick(): void {
+    alert('Ooops! An error occurred.');
+    // alert('Hello World!');
+  }
+
+  public async onSync(): Promise<void> {
+    const result = await LiveUpdate.sync();
+    if (result.nextBundleId) {
+        await LiveUpdate.reload();
+    }
+  }
 }
